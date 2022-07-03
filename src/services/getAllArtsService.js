2410a684a4f7347508bda1e868479d88a8482f0b
Negarnbkhti42 <norob35@gmail.com>
2022-07-03 12:04:12 +0430

    add responsivity and hover effect

--- src/services/getAllArtsService.js
+++ src/services/getAllArtsService.js
@@ -1,7 +1,7 @@
 import http from "./http";
 
 export function getAllArts() {
-    return http.get("/artworks")
+    return http.get("/artworks?page=1&limit=30")
         .then(res => res.data)
         .catch(err => err);
 };
